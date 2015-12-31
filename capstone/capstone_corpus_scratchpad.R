2d84dc77033de1c2836f71a39cbce49e62b03143
sdevine188 <sdevine188@gmail.com>
2015-12-31 15:25:53 -0500

    updated

--- capstone/capstone_corpus_scratchpad.R
+++ capstone/capstone_corpus_scratchpad.R
@@ -5,7 +5,11 @@ library(dplyr)
 library(ggplot2)
 library(textir)
 
+# personal computer
 setwd("C:/Users/Stephen/Desktop/R/coursera_code/capstone")
+
+# work computer
+setwd("H:/R/coursera_code/capstone")
 
 # read in twitter data
 con <- file("en_US/en_US.twitter.txt", "r")
@@ -139,8 +143,7 @@ news_keywords <- tfidf(news_dtm)
 head(news_keywords)
 news_keywords
 
-news_tdm_tfidf <- TermDocumentMatrix(news_corpus2, 
-                control = list(weighting = weightTfIdf))
+news_tdm_tfidf <- TermDocumentMatrix(news_corpus2, control = list(weighting = weightTfIdf))
 inspect(news_tdm_tfidf[82500:82600, 1:10])
 inspect(news_tdm_tfidf[25307:25307, 1:10000])
 
@@ -149,13 +152,12 @@ news_tdm_row_sums <- as.matrix(slam::row_sums(news_tdm, na.rm=TRUE))
 
 # convert to a dataframe
 news_tdm_tfidf_row_sums <- as.matrix(slam::row_sums(news_tdm_tfidf, na.rm=TRUE))
-news_tdm_tfidf_row_mean <- as.matrix(slam::row_sums(news_tdm_tfidf, na.rm=TRUE) / length(news_corpus2))
 
-news_tfidf  <- data.frame(term = rownames(news_tdm_tfidf_row_sums), sum = news_tdm_tfidf_row_sums[ , 1], 
+news_tfidf  <- data.frame(term = rownames(news_tdm_tfidf_row_sums), tfidf_sum = news_tdm_tfidf_row_sums[ , 1], 
                           freq = news_tdm_row_sums[ , 1])
-news_tfidf$avg <- news_tfidf$sum / news_tfidf$freq
+news_tfidf$avg <- news_tfidf$tfidf_sum / news_tfidf$freq
 
-# for some reason about 10% of terms have tf-idf score > 1
+# for some reason about 10% of terms have tf-idf score > 1 (nevermind: tf-idf can be greater than 1)
 rownames(news_tfidf) <- NULL
 news_tfidf  <- arrange(news_tfidf , desc(avg))
 head(news_tfidf, 10)
@@ -163,3 +165,49 @@ head(news_tfidf, 10)
 news_tfidf$term[which(news_tfidf$avg > 1)]
 unique(news_tfidf$freq[which(news_tfidf$avg > 1)])
 filter(news_tfidf, term == "earplugs")
+
+# arrange news_tfidf by descending freq
+news_tfidf <- arrange(news_tfidf, desc(freq))
+head(news_tfidf, 20)
+
+# try using a smaller corpus to see actual docs where a term gets tf-idf > 1 (nevermind: tf-idf can be greater than 1)
+news_corpus2[[1]]
+as.character(news_corpus2[[1]])
+length(news_corpus2)
+
+news_corpus3 <- news_corpus2[1:10]
+length(news_corpus3)
+news_corpus3[[1]]
+as.character(news_corpus3[[1]])
+
+# build small tdm with frequency weighting
+news3_tdm <- TermDocumentMatrix(news_corpus3)
+inspect(news3_tdm[1:10, ])
+
+# build small tdm with tf-idf weighting
+news3_tdm_tfidf <- TermDocumentMatrix(news_corpus3, control = list(weighting = weightTfIdf))
+inspect(news3_tdm_tfidf[1:10, ])
+news3_tdm_row_sums <- as.matrix(slam::row_sums(news3_tdm, na.rm=TRUE))
+
+# get avg tfidf
+news3_tdm_tfidf_row_sums <- as.matrix(slam::row_sums(news3_tdm_tfidf, na.rm=TRUE))
+
+news3_tfidf  <- data.frame(term = rownames(news3_tdm_tfidf_row_sums), sum = news3_tdm_tfidf_row_sums[ , 1], 
+                          freq = news3_tdm_row_sums[ , 1])
+news3_tfidf$avg <- news3_tfidf$sum / news3_tfidf$freq
+head(news3_tfidf)
+max(news3_tfidf$avg)
+news3_tfidf <- arrange(news3_tfidf, desc(avg))
+nrow(news3_tfidf)
+
+# terms "detroit" and "charlevoix" have avg tfidf > 1
+inspect(news3_tdm)
+inspect(news3_tdm[150:217, ])
+inspect(news3_tdm[50:55, ]) # detroit only appears in one document
+
+inspect(news3_tdm_tfidf[50:55, ]) # detroit has a 1.66 tf-idf for that document?! - nevermind tf-idf can be greater than 1
+
+# build my own tf-idf scores
+
+
+
